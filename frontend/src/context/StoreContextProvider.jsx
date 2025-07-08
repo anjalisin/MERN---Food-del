ad64eea61b1c2e09282381265778df245d57bbdc
anjalisin <anjalisinghh8532@gmail.com>
2025-07-08 12:44:21 +0530

    Updated StoreContextProvider

--- frontend/src/context/StoreContextProvider.jsx
+++ frontend/src/context/StoreContextProvider.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const StoreContextProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = import.meta.env.VITE_API_URL;
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
 
